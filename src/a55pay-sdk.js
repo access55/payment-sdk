e5c9a27cc67cab62d10362453449982d489cdfb4
Leandro Silva <leolimasilva123@gmail.com>
2025-08-14 10:58:28 -0300

    refactor: Remove unnecessary field from payment data

--- src/a55pay-sdk.js
+++ src/a55pay-sdk.js
@@ -32,7 +32,6 @@
         payer_name: a55Data.customer?.name,
         payer_email: a55Data.customer?.email,
         installment_count: a55Data.installment_count,
-        installment_value: a55Data.value,
         cell_phone: userData.phone?.replace(/\D/g, ''),
         card: {
           holder_name: userData.holder,
